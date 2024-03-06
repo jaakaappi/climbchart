c403372cd0dd23b734f18e2706fd33ab61471c5a
Esa Niemi <esa.niemi@protonmail.com>
2024-03-06 22:53:37 +0200

    Somehow fixed the colors

--- climbchart-client/src/App.tsx
+++ climbchart-client/src/App.tsx
@@ -320,9 +320,6 @@ const CustomColorBar = (props) => {
     "7b": "#C62D37",
     "7b+": "#C62D37",
   };
-  //@ts-ignore
-  console.log(gradeColors);
-  console.log(props.grade);
 
   //@ts-ignore
   return <Rectangle {...props} fill={gradeColors[props.grade]} />;
